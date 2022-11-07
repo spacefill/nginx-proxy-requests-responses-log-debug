d92b0f4f80aaedebb111478a5a3dce4f85d407c4
Stéphane Klein <contact@stephane-klein.info>
2022-11-07 14:35:30 +0100

    First import

---
+++ README.md
@@ -0,0 +1,9 @@
+# nginx proxy to debug requests and responses
+
+```sh
+$ docker compose build
+```
+
+```sh
+$ docker compose up -d
+```
---
+++ rest-api/main.py
@@ -0,0 +1,27 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
