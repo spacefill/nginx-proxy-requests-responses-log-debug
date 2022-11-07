f1771d4bf2847d046f65a6a2373739678b1414fd
Stéphane Klein <contact@stephane-klein.info>
2022-11-07 15:13:25 +0100

    Don't log when X-Spacefill-Debug response header contains "no"

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # nginx proxy to debug requests and responses
 
+requests + responses header, path, payload are sent to stdout in json format.
+
 ```sh
 $ docker compose build
 ```
--- rest-api/main.py
+++ rest-api/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 
 class Item(BaseModel):
@@ -15,6 +16,16 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/not_debug")
+async def not_debug():
+    return JSONResponse(
+        content={"Not": "Debug"},
+        headers={
+            "X-Spacefill-Debug": "no"
+        }
+    )
 
 
 @app.get("/items/{item_id}")
